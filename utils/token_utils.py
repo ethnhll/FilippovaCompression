6e2132160a537626ad993b6c8f455017b43db67b
Ethan Hill <hill.1303@gmail.com>
2015-04-01 14:19:29 -0400

    fiddled with the util again

--- utils/token_utils.py
+++ utils/token_utils.py
@@ -1,11 +1,16 @@
 __author__ = 'Ethan Hill'
 import nltk
+from collections import namedtuple
 from nltk.tokenize import sent_tokenize, word_tokenize
 
+WordTag = namedtuple('WordTag', 'word, tag')
 
-def tokenize_input(text):
+
+def tokenize_text(text):
     return [word_tokenize(sentence) for sentence in sent_tokenize(text)]
 
 
-def pos_tag_sentences(tokenized_sentences):
-    return [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
+def pos_tag_text(text):
+    tokenized = tokenize_text(text)
+    return [WordTag(w, t) for sent in tokenized for w, t in nltk.pos_tag(sent)]
+

0b9c601cf587a6d10c7d952c032a7512b800b193
withp <james.burgess11011@gmail.com>
2015-04-26 10:03:02 -0400

    Clustering now has an even wider centroid (and dropped clusters of size 1)

--- utils/cluster/src/sentenceCluster/SimClusterMain.java
+++ utils/cluster/src/sentenceCluster/SimClusterMain.java
@@ -5,9 +5,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
 
 //author: James Burgess
@@ -22,16 +20,15 @@ public class SimClusterMain {
 		Set<String> stoplist = SimilarityClusterer.loadStoplist(args[1]);
 		List<List<String>> clusterSents = new ArrayList<List<String>>();
 		for(String s : inputSents) {
-			clusterSents.add(SimilarityClusterer.sanitize(s,stoplist,false));
+			clusterSents.add(SimilarityClusterer.sanitize(s,stoplist,true));
 		}
-		//discourage articles from self-grouping
-		Collections.shuffle(clusterSents, new Random(0));
+		//discourage articles from self-grouping?
+		//Collections.shuffle(clusterSents, new Random(0));
 		
 		//and cluster them
-		
 		SimilarityClusterer c = new SimilarityClusterer(clusterSents);
-		int width = 9;
-		double thresh = 0.01;
+		int width = 25;
+		double thresh = 0.1;
 		if(args.length > 3) {
 			width = Integer.parseInt(args[2]);
 			thresh = Double.parseDouble(args[3]);
@@ -40,17 +37,17 @@ public class SimClusterMain {
 		c.setWidth(width);
 		List<List<Integer>> clusters = c.cluster();
 		for(List<Integer> i : clusters) {
-			
-			for(int j = 0; j < i.size(); j++) {
-				System.out.print(i.get(j));
-				if(j+1 < i.size()) {
-				System.out.print(',');
-				} else {
-					System.out.print('\n');
+			if(i.size() > 1) {
+				//drop overly unique sentences
+				for(int j = 0; j < i.size(); j++) {
+					System.out.print(i.get(j));
+					if(j+1 < i.size()) {
+					System.out.print(',');
+					} else {
+						System.out.print('\n');
+					}
 				}
 			}
 		}
 	}
-	
-
 }

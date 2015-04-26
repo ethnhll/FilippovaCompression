47a2711bd786f61423941467db3f50a5caa989ab
jchang358 <jchang358@hotmail.com>
2015-04-26 10:03:09 -0400

    more legible output encode to ascii
    formated the output to look nicer added auto ascii encoding

--- __main__.py
+++ __main__.py
@@ -23,7 +23,7 @@ def main():
     with open(arguments.text_file) as text_file:
         unprocessed = ''.join(text_file.readlines())
         unprocessed = unprocessed.replace('\n', ' ')
-
+        unprocessed = unprocessed.decode('utf-8',errors = 'ignore').encode('ascii')
         split_sentences = token_utils.sent_tokenize(unprocessed)
         with open('split_tmp', 'w') as temp_file:
             temp_file.write('\n'.join(split_sentences))
@@ -37,12 +37,18 @@ def main():
                 [token_utils.prepare_word_info(split_sentences[index], index) for index in cluster])
     with open(arguments.stop_words) as stop_word_file:
         stop_word_list = stop_word_file.readlines()
-        for sentences in sentence_clusters:
+        print 'FILE SUMMARY'
+        for cluster_id, sentences in enumerate(sentence_clusters):
             word_graph = Word_Graph(sentences, stop_word_list)
             word_graph.process_graph()
             word_graph.reweight_edges('baseline')
             paths = word_graph.k_shortest_path(9,50)
-            word_graph.print_graph()
+            if paths:
+                top_sentence = ' '.join([word_graph.graph[i].word for i in paths[0][0]])
+            else:
+                top_sentence = 'No Sentence'
+            print '\t%d. %s'%(cluster_id,top_sentence)
+            word_graph.print_graph(cluster_id)
 
 
 
